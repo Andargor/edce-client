f6e78b72f62ce43ac54e6723d1f1629b6f4325d4
dmacesic <dejan@lofts.net>
2016-09-03 01:22:18 -0400

    Using key-value pairs instead of edict iteration for >3.4 compatibility

--- player_dump.py
+++ player_dump.py
@@ -2,8 +2,8 @@ import sys
 import edce.config
 
 if sys.version_info.major < 3:
-	print("You need to use Python 3.x, e.g. python3 <filename>")
-	exit()
+    print("You need to use Python 3.x, e.g. python3 <filename>")
+    exit()
 
 import edce.util
 import json
@@ -13,47 +13,48 @@ def alignStr(title, word):
     return "{:<40s}{:<40s}".format(title, word)
 
 def alignNum(title, number):
-    return "{:<40s}{:<40d}".format(title, number)	
+    return "{:<40s}{:<40d}".format(title, number)    
 
 player_json = ''
 try:
-	with open(edce.config.getString('paths', 'last_file'), "r") as f:
-			player_json = f.readline()
-			f.close()
+    with open(edce.config.getString('paths', 'last_file'), "r") as f:
+            player_json = f.readline()
+            f.close()
 except:
-	"Please run edce_client.py at least once, so that it may create a last.json file."
-	exit()
-		
+    "Please run edce_client.py at least once, so that it may create a last.json file."
+    exit()
+        
 data = edce.util.edict(player_json)
 
 station = ""
 if data.commander.docked:
-	station = "/" + data.lastStarport.name
-	
+    station = "/" + data.lastStarport.name
+    
 print(alignStr("CMDR:",data.commander.name))
 print(alignStr("System:",data.lastSystem.name + station))
 print(alignNum("Credits:",data.commander.credits))
 
 if "ship" in data:
-	print(alignStr("Ship:",data.ship.name))
+    print(alignStr("Ship:",data.ship.name))
 
 if "ships" in data:
-	print("\n========== All ships: ==========")
-	
-	all_ships = {}
-	for ship in data.ships:
-		starsystem = data.ships[ship].starsystem
-		starsystem_station = data.ships[ship].station
-		if starsystem and starsystem_station:
-		        station = starsystem.name + "/" + starsystem_station.name
-		        if not station in all_ships:
-			        all_ships[station]=[]
-		        all_ships[station].append(data.ships[ship].name)
+    print("\n========== All ships: ==========")
+    
+    all_ships = {}
+    for ship, shipEntry in data.ships.items():
+        starsystem = shipEntry.starsystem
+        starsystem_station = shipEntry.station
+        if starsystem and starsystem_station:
+            station = starsystem.name + "/" + starsystem_station.name
+            if not station in all_ships:
+                all_ships[station]=[]
+            
+            all_ships[station].append(shipEntry.name)
 
-	for s in OrderedDict(sorted(all_ships.items())):
-		print(alignStr(s,", ".join(all_ships[s])))
-		
+    for s in OrderedDict(sorted(all_ships.items())):
+        print(alignStr(s,", ".join(all_ships[s])))
+        
 if "stats" in data and "explore" in data.stats and "lastVisitedStarSystems" in data.stats.explore:
-	print("\n========== Last Visited: ==========")
-	for s in data.stats.explore.lastVisitedStarSystems:
-		print(s)
+    print("\n========== Last Visited: ==========")
+    for s in data.stats.explore.lastVisitedStarSystems:
+        print(s)
